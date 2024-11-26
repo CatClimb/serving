ce0af9c2dc5d6300b80b900054981c835f595d4f
CatClimb <1104432863@qq.com>
2024-11-26 13:38:08 +0800

    修改头

--- tensorflow_serving/util/net_http/server/internal/evhttp_request.h
+++ tensorflow_serving/util/net_http/server/internal/evhttp_request.h
@@ -80,7 +80,7 @@ class EvHTTPRequest final : public ServerRequestInterface {
   void WriteResponseBytes(const char* data, int64_t size) override;
 
   void WriteResponseString(absl::string_view data) override;
-  void StreamResponse(absl::string_view data,HTTPStatusCode status) = 0;
+  void StreamResponse(absl::string_view data,HTTPStatusCode status) override;
   std::unique_ptr<char[], ServerRequestInterface::BlockDeleter>
   ReadRequestBytes(int64_t* size) override;
 

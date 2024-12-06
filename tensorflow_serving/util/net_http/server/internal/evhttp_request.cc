d6bafbe7e256896407ff7d8e7fc4c3d31806e269
CatClimb <1104432863@qq.com>
2024-12-06 11:22:30 +0800

    修复语法1-7

--- tensorflow_serving/util/net_http/server/internal/evhttp_request.cc
+++ tensorflow_serving/util/net_http/server/internal/evhttp_request.cc
@@ -178,7 +178,7 @@ void EvHTTPRequest::StreamResponse(absl::string_view data,HTTPStatusCode status,
   replay_chunk_cb(NULL, static_cast<void*>(this));
 }
 static void EvHTTPRequest::replay_chunk_static_callback(evhttp_connection* conn, void* arg) {
-    
+    EvHTTPRequest* self = static_cast<EvHTTPRequest*>(arg);  // 通过 arg 获取到当前对象的实例
     self->replay_chunk_cb(conn, arg);  // 调用成员函数
 }
 /**响应块回调 */
